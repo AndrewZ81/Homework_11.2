8e9ea379911ee4d507f5cadd4241b63adf5658a5
Andrew Zaiko <andrewzaiko@gmail.com>
2022-07-16 18:00:29 +0300

    Добавил маршрут для поиска по имени кандидата

--- app.py
+++ app.py
@@ -25,6 +25,9 @@ app.add_url_rule("/candidates/<int:id>", view_func=utils.get_by_pk)
 # Создаём маршрут для поиска кандидатов по навыку
 app.add_url_rule("/skills/<skill>", view_func=utils.get_by_skill)
 
+# Создаём маршрут для поиска кандидатов по имени
+app.add_url_rule("/candidates/<name>", view_func=utils.get_by_name)
+
 # Запускаем сервер
 if __name__ == "__main__":
     app.run()
